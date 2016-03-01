fce2d8e9cb5531da8ae84c7d3bd26c15196ac178
Jason Awbrey and Jordan Meier <jbirdmeier@gmail.com>
2016-03-01 10:40:25 -0800

    Add red stage tests for Student save, getAll, and deleteAll

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -17,5 +17,82 @@
             Course::deleteAll();
             Department::deleteAll();
         }
+
+        function test_save()
+        {
+            //Arrange
+            $name = "History";
+            $test_department = new Department($name);
+            $test_department->save();
+
+            $name = "Jonas Frizzle";
+            $enrollment_date = "2016-02-10";
+            $dept_id = $test_department->getId();
+            $test_student = new Student($name, $enrollment_date, $dept_id);
+            $test_student->save();
+
+            //Act
+            $result = Student::getAll();
+
+            //Assert
+
+            $this->assertEquals($test_student, $result[0]);
+
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "History";
+            $test_department = new Department($name);
+            $test_department->save();
+
+            $name = "Jonas Frizzle";
+            $enrollment_date = "2016-02-10";
+            $dept_id = $test_department->getId();
+            $test_student = new Student($name, $enrollment_date, $dept_id);
+            $test_student->save();
+
+            $name2 = "Marsha Grizzle";
+            $enrollment_date2 = "2016-02-10";
+            $dept_id2 = $test_department->getId();
+            $test_student2 = new Student($name2, $enrollment_date2, $dept_id2);
+            $test_student2->save();
+
+            //Act
+            $result = Student::getAll();
+
+            //Assert
+            $this->assertEquals([$test_student, $test_student2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "History";
+            $test_department = new Department($name);
+            $test_department->save();
+
+            $name = "Jonas Frizzle";
+            $enrollment_date = "2016-02-10";
+            $dept_id = $test_department->getId();
+            $test_student = new Student($name, $enrollment_date, $dept_id);
+            $test_student->save();
+
+            $name2 = "Marsha Grizzle";
+            $enrollment_date2 = "2016-02-10";
+            $dept_id2 = $test_department->getId();
+            $test_student2 = new Student($name2, $enrollment_date2, $dept_id2);
+            $test_student2->save();
+
+
+            //Act
+            Student::deleteAll();
+            $result = Student::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
 	}
 ?>
