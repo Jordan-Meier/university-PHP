8c96865e6a3cdde8977a4e2d70a906ecff43c107
Jason Awbrey and Jordan Meier <jason.s.awbrey@gmail.com>
2016-03-01 09:08:03 -0800

    set up test files

---
+++ tests/CourseTest.php
@@ -0,0 +1,24 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Student.php";
+    require_once "src/Course.php";
+    require_once "src/Department.php";
+
+    $server = 'mysql:host=localhost;dbname=university_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CourseTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Student::deleteAll();
+            Course::deleteAll();
+            Department::deleteAll();
+        }
+	}
+?>
