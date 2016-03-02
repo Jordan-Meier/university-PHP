b1fc4e07de32ad0390f8097c5868f523fac96a2a
Jason Awbrey and Jordan Meier <jason.s.awbrey@gmail.com>
2016-03-01 16:08:05 -0800

    Add initial twig page functionality for index, departments, and students

--- app/app.php
+++ app/app.php
@@ -1,13 +1,58 @@
-<?php 
+<?php
 
-	require_once __DIR__.'/../vendor/autoload.php'; 
+	require_once __DIR__.'/../vendor/autoload.php';
 
-	$app = new Silex\Application(); 
+	$server = 'mysql:host=localhost;dbname=university';
+	$username = 'root';
+	$password = 'root';
+	$DB = new PDO($server, $username, $password);
 
-	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views')); 
+	$app = new Silex\Application();
+	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-	$app->get('/', function(){return 'Hello, World!';}); 
+	$app->get('/', function() use ($app)
+	{
+		return $app['twig']->render('index.html.twig');
+	});
 
-	return $app; 
+	$app->get('/departments', function() use ($app)
+	{
+		$departments = Department::getAll();
+		return $app['twig']->render('departments.html.twig', array('departments' => $departments));
+	});
+
+	$app->post('/departments', function() use ($app)
+	{
+		$new_department = new Department($_POST['name']);
+		$new_department->save();
+
+		$departments = Department::getAll();
+		return $app['twig']->render('departments.html.twig', array('departments' => $departments));
+	});
+
+	$app->get('/courses', function() use ($app)
+	{
+		$courses = Course::getAll();
+		return $app['twig']->render('courses.html.twig', array('courses' => $courses));
+	});
+
+	$app->get('/students', function() use ($app)
+	{
+		$students = Student::getAll();
+		$departments = Department::getAll();
+		return $app['twig']->render('students.html.twig', array('students' => $students, 'departments' => $departments));
+	});
+
+	$app->post('/students', function() use ($app)
+	{
+		$new_student = new Student($_POST['name'], $_POST['enrollment_date'], $_POST['dept_id']);
+		$new_student->save();
+
+		$students = Student::getAll();
+		$departments = Department::getAll();
+		return $app['twig']->render('students.html.twig', array('students' => $students, 'departments' => $departments));
+	});
+
+	return $app;
 
 ?>
